ede55b415782fe02c215f131f16020cfd2306444
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-04-27 21:34:52 -0300

    fix(test): add test sample using vcr

---
+++ tests/api/endpoints/test_using_vcr/test_users.py
@@ -0,0 +1,22 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+import pytest
+from vcr_showcase.models import User, UserList
+
+
+@pytest.mark.vcr
+def test_get_all_users(
+    test_client: TestClient
+) -> None:
+    response = test_client.get("/api/users/")
+    assert response.status_code == status.HTTP_200_OK
+    assert UserList.validate_python(response.json())
+
+
+@pytest.mark.vcr
+def test_get_user_by_id(
+    test_client: TestClient, user_id: int
+) -> None:
+    response = test_client.get(f"/api/users/{user_id}")
+    assert response.status_code == status.HTTP_200_OK
+    assert User.model_validate(response.json())
