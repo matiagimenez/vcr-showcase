c7bbf99397408baa63970838f6fd5bee4065c16f
Matias Gimenez <matiasgimenez.dev@gmail.com>
2025-04-27 21:34:38 -0300

    fix(test): remove debug print statement

--- tests/api/endpoints/test_using_requests_mock/test_users.py
+++ tests/api/endpoints/test_using_requests_mock/test_users.py
@@ -18,6 +18,5 @@ def test_get_user_by_id(
     test_client: TestClient, user_id: int, user_as_dict: dict[str, Any]
 ) -> None:
     response = test_client.get(f"/api/users/{user_id}")
-    print(f"response: {response.json()}")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == user_as_dict
